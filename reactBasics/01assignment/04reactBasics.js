1e52f393fa355d0ced28a081baf991cbd0f41f90
Ranjith-singh <singhranjith970@gmail.com>
2026-02-04 11:56:24 +0530

    Assignments and projects using ReactJs

---
+++ reactBasics/01assignment/04reactBasics.js
@@ -0,0 +1,6 @@
+export default order= (packOfSock, tShirts)=>{
+    const sockCost= 10;
+    const tShirtCost= 8;
+    const productCost= packOfSock*sockCost+ tShirts*tShirtCost;
+    console.log(productCost);
+}
