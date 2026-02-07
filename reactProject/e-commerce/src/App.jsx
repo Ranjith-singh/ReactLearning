042587cb45c657fb3a97e8a43a4dafdb400d6157
Ranjith-singh <singhranjith970@gmail.com>
2026-02-07 11:44:39 +0530

    initialize e-commerce project with React and Vite Created a jsx and Styling for the Home, Checkout, Tracking, orders  and created Header Component

---
+++ reactProject/e-commerce/src/App.jsx
@@ -0,0 +1,22 @@
+import { HomePage } from './pages/HomePage'
+import Checkout from './pages/Checkout'
+import { Routes, Route } from 'react-router'
+import { Orders } from './pages/Orders'
+import { Tracking } from './pages/Tracking'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path='checkout' element={<Checkout />} />
+        <Route path='orders' element={<Orders />} />
+        <Route path='tracking' element={<Tracking />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
